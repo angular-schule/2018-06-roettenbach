686093f2425e3d61ff93958b9e8b69743d2dd64f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-07-12 10:09:38 +0200

    complex page object

---
+++ book-rating/e2e/src/google.e2e-spec.ts
@@ -0,0 +1,46 @@
+import { GooglePage } from './google.po';
+import { browser } from 'protractor';
+import { GoogleResultsPage } from './google-results.po';
+
+describe('Google', () => {
+
+  beforeAll(() => {
+    browser.waitForAngularEnabled(false);
+  });
+
+  let googlePage: GooglePage;
+  let resultsPage: GoogleResultsPage;
+
+  beforeEach(() => {
+    googlePage = new GooglePage();
+
+    resultsPage = googlePage
+      .navigateTo()
+      .search('Angular');
+  });
+
+  /*
+  it('should display 10 results', () => {
+    browser.sleep(2000); // TODO, wait for known element to speed up the test
+
+    const results = resultsPage.getResults();
+    expect(results.count()).toBe(10);
+  });
+  */
+
+
+  it('should show knowledge panel with description', () => {
+    const panel = resultsPage.getKnowledgePanel();
+    expect(panel.getText())
+      .toContain('Angular is a TypeScript-based open-source front-end web application platform');
+  });
+
+ it('should show Angular.io as first result', () => {
+    const url = resultsPage.getFirstResultUrl().getText();
+    expect(url).toBe('https://angular.io/');
+  });
+
+  afterAll(() => {
+    browser.waitForAngularEnabled(true);
+  });
+});
