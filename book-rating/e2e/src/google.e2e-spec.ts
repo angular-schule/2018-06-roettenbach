0beb15343e04a4b6d32950413eafb98033026f83
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-07-16 13:51:33 +0200

    adds debug command for protractor

--- book-rating/e2e/src/google.e2e-spec.ts
+++ book-rating/e2e/src/google.e2e-spec.ts
@@ -30,7 +30,6 @@ describe('Google', () => {
 
 
   it('should show knowledge panel with description', () => {
-    debugger
     const panel = resultsPage.getKnowledgePanel();
     expect(panel.getText())
       .toContain('Angular is a TypeScript-based open-source front-end web application platform');
